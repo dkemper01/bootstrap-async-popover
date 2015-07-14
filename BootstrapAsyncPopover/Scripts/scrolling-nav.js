b4eba8ca8e3ed26645af177462a02e7be7cf7119
dkemper01 <kemper001@hotmail.com>
2015-07-13 23:02:43 -0500

    Added github redirect.

--- BootstrapAsyncPopover/Scripts/scrolling-nav.js
+++ BootstrapAsyncPopover/Scripts/scrolling-nav.js
@@ -3,9 +3,13 @@ $(window).scroll(function() {
     if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
         $(".navbar i.fa").removeClass("fa-github").addClass("fa-level-up");
+        $(".navbar i.fa").off("click");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
         $(".navbar i.fa").removeClass("fa-level-up").addClass("fa-github");
+        $(".navbar i.fa").on("click", function () {
+            window.location.href = "https://github.com/dkemper01/bootstrap-async-popover";
+        });
     }
 });
 
