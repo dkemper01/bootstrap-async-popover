993ce0c05e3b2d3a69aa1f83fcdb017cbd657785
dkemper01 <kemper001@hotmail.com>
2015-07-13 21:58:30 -0500

    Initial commit.
    The bootstrap async popover extension demo project, initial commit.

---
+++ BootstrapAsyncPopover/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BootstrapAsyncPopover.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult FetchDemoTooltipContent(string entry, int gmtOffsetHours, int gmtOffsetMinutes)
+        {
+            string htmlToReturn = "<p>You didn't enter anything in this field yet.</p>";
+            System.DateTime callerDateTime = System.DateTime.UtcNow.AddHours(gmtOffsetHours).AddMinutes(gmtOffsetMinutes);
+
+            // Simulate long running task here ...
+            //
+            System.Threading.Thread.Sleep(500);
+
+            if (!String.IsNullOrEmpty(entry))
+            {
+                htmlToReturn = String.Format("<p>On {0} at {1}, you entered: </p> <p></p> <p>{2}</p>", callerDateTime.ToShortDateString(), callerDateTime.ToShortTimeString(), HttpUtility.HtmlAttributeEncode(entry));
+            }
+
+            return new JsonResult { Data = htmlToReturn, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+    }
+}
---
+++ BootstrapAsyncPopover/Scripts/scrolling-nav.js
@@ -0,0 +1,21 @@
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+        $(".navbar i.fa").removeClass("fa-github").addClass("fa-level-up");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        $(".navbar i.fa").removeClass("fa-level-up").addClass("fa-github");
+    }
+});
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
